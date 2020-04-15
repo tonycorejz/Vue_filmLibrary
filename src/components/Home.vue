790a477d98f4805b392e67bd71eee8f9ccc357a9
tonycorejz <mr.korovin-mihail@ya.ru>
2020-04-15 09:04:06 +0800

    Второй коммит) Добавил возможность добавлять фильмы/сериалы

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -14,6 +14,39 @@
                     v-model="taskDescription"
                     @keyup.enter="newTask"
                 )
+                .option-list
+                    input.what-watch--radio(
+                      type="radio"
+                      id="radioFilm"
+                      value="Film"
+                      v-model="whatWatch"
+                    )
+                    label(
+                      for="radioFilm"
+                    ) Film
+                    input.what-watch--radio(
+                      type="radio"
+                      id="radioSerial"
+                      value="Serial"
+                      v-model="whatWatch"
+                    )
+                    label(
+                      for="radioSerial"
+                    ) Serial
+                .total-time
+                  .total-time__film(
+                    v-if="whatWatch === 'Film'"
+                  )
+                    span Total Film Times
+                  .total-time__Serial(
+                    v-if="whatWatch === 'Serial'"
+                  )
+                    span Total Serial Times
+                .tag-list
+                  .ui-tag__wrapper
+                    .ui-tag
+                      span.tag-title Dogs
+                      span.button-close
         section
             .conteiner
                 .task-item(
@@ -69,24 +102,44 @@ export default {
   },
   methods: {
     newTask () {
+      if (this.taskTitle === '') return
+
       this.tasks.push({
         id: this.taskId,
-        title: task.taskTitle,
+        title: this.taskTitle,
         description: this.taskDescription,
         whatWatch: this.whatWatch,
         completed: false,
         editing: false
       })
+
+      // Reset
+      this.taskId += 1
+      this.taskTitle = ''
+      this.taskDescription = ''
     }
   }
 }
 </script>
 
 <style lang="stylus" scoped>
+.option-list
+    display flex
+    margin-bottom 20px
+    .what-watch--radio
+        margin-right  12px
+    label
+        margin-right  20px
+        &:last-child
+            margin-right  0
+
 .task-item
     margin-bottom 20px
     &:last-child
         margin-bottom 0
+
+.total-time
+    margin-bottom 20px
 
 .ui-label
     margin-right 8px
